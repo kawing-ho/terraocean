078654aa2ee6614ba804488e43b900efd56de8f4
kawing-ho <gavin_kw@hotmail.com>
2018-12-19 19:19:50 +1100

    update

--- playground.tf
+++ playground.tf
@@ -1,16 +1,32 @@
+variable "do_token" {}
+
 provider "digitalocean" {
 
         # included in the super secret local variables file
-        token = "${var.do_access_token}"
+        token = "${var.do_token}"
         ssh_fingerprint = "dd:2a:04:8f:f8:69:64:94:a8:de:86:f2:1a:af:b5:3c"
-
-
+        private_key = "~/.ssh/terraform_key"
 }
 
-resource "digitalocean-droplet" "playground" {
+resource "digitalocean_droplet" "playground" {
 
         image = "ubuntu-18-04-x64"
         name = "playground"
         region = "sgp1"
         size = "s-1vcpu-1gb"
+        ssh_keys = [ "23705689", "23728621" ]
+
+        connection {
+                user = "root"
+                type = "ssh"
+                private_key = "${file(var.private_key)}"
+                timeout = "2m"
+
+        }
+
+
+        provisioner "remote-exec" {
+
+
+        }
 }
