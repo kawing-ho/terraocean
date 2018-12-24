25b293a1bc57956e64af3d1bbae4fc18edb6812c
kawing-ho <kawing-ho@users.noreply.github.com>
2018-12-24 12:49:42 +1100

    added SSH host file manipulation

--- playground.tf
+++ playground.tf
@@ -1,7 +1,9 @@
+/* Secret variables that I'm not gonna commit to the repo lol */
 variable "do_token" {}
-variable "image" {}
+variable "pull_image" {}
 variable "repo" {}
 variable "HOME" {}
+
 locals {
         keyfile = "${var.HOME}/.ssh/terraform_key"
 }
@@ -12,15 +14,19 @@ provider "digitalocean" {
         token = "${var.do_token}"
 }
 
+data "digitalocean_image" "playground" {
+        name = "playground-img-1545567755"
+}
+
 resource "digitalocean_droplet" "playground" {
 
         # we use our own packer-baked custom image
-        image = "${var.image}"
+        image = "${data.digitalocean_image.playground.image}"
         # image = "ubuntu-18-04-x64"
         name = "playground"
         region = "sgp1"
 
-        # s-4vcpu-8gb (example format) 
+        # s-4vcpu-8gb (example format, $40/month ooft) 
         size = "s-4vcpu-8gb"
         ssh_keys = [ "23705689", "23728621" ]
 
@@ -31,6 +37,7 @@ resource "digitalocean_droplet" "playground" {
                 private_key = "${file("${local.keyfile}")}"
         }
 
+        # copy repo keys over
         provisioner "file" {
                 source = "${var.HOME}/.ssh/id_rsa"
                 destination = ".ssh/id_rsa"
@@ -38,21 +45,37 @@ resource "digitalocean_droplet" "playground" {
 
         provisioner "file" {
                 source = "${var.HOME}/.ssh/id_rsa.pub"
-                destination = ".ssh/id_rsa/pub"
+                destination = ".ssh/id_rsa.pub"
         }
 
         provisioner "remote-exec" {
 
                 # clone the repository (requires repo keys)
                 inline = [
-                        "git clone --single-branch --branch staging ${var.repo}"
+                        "touch .hushlogin",
+                        "ssh-keyscan -H github.com >> ~/.ssh/known_hosts",
+                        "git clone --single-branch --branch staging ${var.repo}",
+                        "docker pull ${var.pull_image}"
                 ]
         }
 
         provisioner "local-exec" {
-                # can be used to update SSH config when done perhaps
+                # update local SSH config file 
                 command = "sed -i \"s/$(ssh -G playground | egrep ^hostname | tr -d [:space:] | tr -d hostname)/${self.ipv4_address}/\" ${var.HOME}/.ssh/config"
         }
+
+        provisioner "local-exec" {
+                # add new IP to known hosts
+                command = "ssh-keyscan -H ${self.ipv4_address} >> ~/.ssh/known_hosts"
+        }
+
+        # DESTROY-TIME PROVISIONER
+        provisioner "local-exec" {
+                # remove IP from known hosts
+                when = "destroy"
+                command = "ssh-keygen -R ${self.ipv4_address}"
+        }
+
 }
 
 output "public_ip" {
